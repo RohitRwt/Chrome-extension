c1e8688e715eae85a8acabb2c8e214b73f8e4e13
Rohit <rawatrohit8171@gmail.com>
2022-07-21 09:20:18 +0530

    added the string templete feature, clear input field and add some styling to list

--- cr.js
+++ cr.js
@@ -4,14 +4,98 @@ const inputEl = document.getElementById("input-el")
 const unEL = document.getElementById("unorder-el")
 
 
-inpButton.addEventListener("click",function(){
-    // myleads.push(inputEl.value)
-    for (i in myleads){
-        unEL.innerHTML +="<li>"+ myleads[i]+"</li>"
-   
-    }
+inpButton.addEventListener("click", function () {
+    myleads.push(inputEl.value)
+    render();
+    inputEl.value = "";
 }
 )
 
- 
+function render(){
+var listitems = "";
+    for (i in myleads) {
+    // listitems += "<li> <a  taget = '_blank' href = '" +myleads[i] +"' >"   +myleads[i]+  " "+  "</li>"
+    // Templete string usage
+    listitems += `<li> 
+    
+    <a href = ${myleads[i]} target = '_blank'> 
 
+    ${myleads[i]} 
+    
+    </a> 
+
+    </li> `
+
+    }
+
+unEL.innerHTML = listitems;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const recipients = "james"
+// const name = "rohit"
+
+// const email = `hey ${recipients}!
+//  how is it going?
+
+// Cheers ${name} `
+// console.log(email)
