42470e3662ee7466fb1b2dcb12ff9a0b18c893cd
Rohit <rawatrohit8171@gmail.com>
2022-07-21 12:51:13 +0530

    Added the chrome tab api and make savetab button

--- cr.js
+++ cr.js
@@ -1,34 +1,45 @@
 const inpButton = document.getElementById("save-btn")
+const delButton = document.getElementById("del-el")
+const savetab = document.getElementById("tab-el")
 let myleads = []
+
+
 const inputEl = document.getElementById("input-el")
 const unEL = document.getElementById("unorder-el")
 
 
-inpButton.addEventListener("click", function () {
-    myleads.push(inputEl.value)
-    render();
-    inputEl.value = "";
+
+const leadsfromlocalstorage = JSON.parse(localStorage.getItem("myleads"))
+if (leadsfromlocalstorage) {
+    myleads = leadsfromlocalstorage;
+    render(myleads);
 }
-)
+savetab.addEventListener("click",function(){
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        myleads.push(tab[0].url)
+    localStorage.setItem("myleads",JSON.stringify(myleads))
+    render(myleads)
+    })
+})
 
-function render(){
-var listitems = "";
-    for (i in myleads) {
-    // listitems += "<li> <a  taget = '_blank' href = '" +myleads[i] +"' >"   +myleads[i]+  " "+  "</li>"
-    // Templete string usage
-    listitems += `<li> 
+
+
+function render(leads) {
+    var listitems = "";
+    for (i in leads) {
+        listitems += `<li> 
+        
+        <a href = ${leads[i]} target = '_blank'> 
     
-    <a href = ${myleads[i]} target = '_blank'> 
-
-    ${myleads[i]} 
+        ${leads[i]} 
+        
+        </a> 
     
-    </a> 
-
-    </li> `
+        </li> `
 
     }
 
-unEL.innerHTML = listitems;
+    unEL.innerHTML = listitems;
 
 
 
@@ -36,66 +47,24 @@ unEL.innerHTML = listitems;
 
 
 
+delButton.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myleads = []
+    render(myleads)
+})
+
+
+inpButton.addEventListener("click", function () {
+    myleads.push(inputEl.value)
+    inputEl.value = "";
+    var ml = JSON.stringify(myleads)
+    localStorage.setItem("myleads", ml)
+
+    render(myleads);
+
+}
+)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const recipients = "james"
-// const name = "rohit"
-
-// const email = `hey ${recipients}!
-//  how is it going?
-
-// Cheers ${name} `
-// console.log(email)
